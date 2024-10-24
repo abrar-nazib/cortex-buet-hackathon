b834b478d3fd29edf77fb9899bb66bc8e6bb55da
shihabul <mdshihabulislam.mte.ruet@gmail.com>
2024-10-24 17:42:27 +0600

    prochur change

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,11 +1,13 @@
 import SearchTrain from "@/components/SearchTrain";
 import React from "react";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Search = () => {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <h1 className="text-primary/80 font-bold text-center text-2xl mb-4 ">Search For Trains</h1>
+      <h1 className="text-primary/80 font-bold text-center text-2xl mb-4 ">
+        Search For Trains
+      </h1>
       <SearchTrain />
     </div>
   );
